d2485a4f4a5362e1c84058aefc030cbfb0d5f305
evnchn <evanchan040511@gmail.com>
2023-02-09 14:40:49 +0800

    Use linkstart.py for dependencies handling

--- zTrimmer.py
+++ zTrimmer.py
@@ -1,16 +1,45 @@
+import_string = \
+'''
 import io
 import os
 import PySimpleGUI as sg
 import sys
-from PIL import Image, ImageTk, ImageOps, ImageGrab, ImageChops
+from PIL import Image, ImageTk, ImageOps, ImageGrab, ImageChops # Pillow
 import time
-import numpy as np
+import numpy as np # numpy
 from io import BytesIO
-import win32clipboard
+import win32clipboard # pywin32
 import webbrowser
 import string
 import ctypes
 import platform
+'''
+
+### LINK START! (https://github.com/evnchn/linkstart.py)
+for line in import_string.splitlines():
+    if "import" in line:
+        print(line)
+        try:
+            exec(line)
+        except:
+            if "#" in line:
+                package_name = line.split("#")[-1]
+            else:
+                splits = line.split("import")
+                if "from" in line:
+                    package_name = splits[0].replace("from","")
+                else:
+                    package_name = splits[1]
+            package_name = package_name.strip()
+            print("Installing {}...".format(package_name))    
+            import subprocess
+            import sys
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+            try:
+                exec(line)
+            except:
+                print("Failed to install {}".format(package_name))
+### DONE
 
 
 def make_dpi_aware():
@@ -42,6 +71,7 @@ print(img.mode)
 layout = [
 
         [sg.Image(key='-IMAGE-', pad=(0, 0))]
+        
          ]
          
 
